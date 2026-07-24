536510008859db7c5c014103199bd35450818241
Bernd Fix <brf@hoi-polloi.org>
2026-07-24 15:19:17 +0200

    Working solution.

--- data/adb/service.d/katzenpost.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-while [ "$(getprop sys.boot_completed)" != "1" ]; do
-    sleep 1
-done
-/data/adb/modules/katzenpost/kpclientd -c /data/adb/modules/katzenpost/client_ws.toml > /data/local/tmp/kpclient.log 2>&1 &
---
+++ data/adb/service.d/kpclientd.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+    sleep 1
+done
+sleep 2
+
+cd /data/adb/modules/kpclientd/
+if [ ! -e "disabled" ]; then
+    ./kpclientd -c client_ws.toml > /data/local/tmp/kpclientd.log 2>&1 &
+fi
+exit 0
