df89603e0dc94b64cdbe9cea9942933cd88e8e2f
Bernd Fix <brf@hoi-polloi.org>
2026-08-07 12:08:23 +0200

    Use local git server.

--- f-droid/local/build.sh
+++ f-droid/local/build.sh
@@ -2,13 +2,20 @@
 
 mkdir $1
 cd $1
+
 fdroid init
+
 mkdir metadata
 cp ../org.hoi_polloi.kpdaemon.yml metadata/
+
 mkdir build
 cd build/
-git clone https://github.com/bfix/kpclientd-android org.hoi_polloi.kpdaemon
-cd ..
+#git clone https://github.com/bfix/kpclientd-android org.hoi_polloi.kpdaemon
+git clone http://127.0.0.1:13000/brf/kpclientd-android org.hoi_polloi.kpdaemon
+cd org.hoi_polloi.kpdaemon
+git switch dev
+cd ../..
+
 cat > config.yml <<EOF
 ---
 sdk_path: /usr/lib/android-sdk
@@ -22,10 +29,13 @@ keypass: gnXL/MMpkVbp/g8O32UGRQetcpcMmaUpkV2WcxuzzrI=
 keydname: CN=saturn.hoi-polloi.org, OU=F-Droid
 use_in_repository_metadata: false
 EOF
+
 mkdir srclibs
 cat > srclibs/katzenpost.yml <<EOF
 Repo: https://github.com/katzenpost/katzenpost
 RepoType: git
 EOF
+
 fdroid build --verbose org.hoi_polloi.kpdaemon
+
 ls -la unsigned
